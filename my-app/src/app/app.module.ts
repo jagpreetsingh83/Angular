ba78802a313f7be61c32fa98d1a6802340f521a4
Obhi, Jagpreet <jagpreet_obhi@manulife.com>
2018-01-11 16:11:30 -0500

    routing for dashboard and heroes

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -8,13 +8,15 @@ import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 import {HeroService} from './hero.service';
 import {MessagesComponent} from './messages/messages.component';
 import {MessageService} from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent
+    AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, AppRoutingModule
   ],
   providers: [
     HeroService, MessageService
