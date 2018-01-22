7278a025c7efad54aef19fedf14355616cfbec24
Obhi, Jagpreet <jagpreet_obhi@manulife.com>
2018-01-21 19:09:55 -0500

    ng-bootstrap

--- tour-of-heroes-ui/src/app/app.component.ts
+++ tour-of-heroes-ui/src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Location} from '@angular/common';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
 
 export class AppComponent {
-  title = 'Tour of Heroes';
-}
 
+  constructor(private location : Location) {}
+
+  title = 'Tour of Heroes';
+
+  isActive(link) : boolean {
+    return this
+      .location
+      .path()
+      .indexOf(link) > -1;
+  }
+}
--- tour-of-heroes-ui/src/app/app.module.ts
+++ tour-of-heroes-ui/src/app/app.module.ts
@@ -13,18 +13,25 @@ import {DashboardComponent} from './dashboard/dashboard.component';
 import {HttpClientModule} from '@angular/common/http';
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDataService} from './in-memory-data.service';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
+import {HeroSearchComponent} from './hero-search/hero-search.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent, HeroSearchComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule, FormsModule, AppRoutingModule, HttpClientModule,
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests and
     // returns simulated server responses. Remove it when a real server is ready to
     // receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false})
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false}),
+    NgbModule.forRoot()
   ],
   providers: [
     HeroService, MessageService
