6db72614be0521036bcf697dcb96a1fe0fb39ce9
Obhi, Jagpreet <jagpreet_obhi@manulife.com>
2018-01-31 12:28:36 -0500

    State Transfer to Phone Item

--- universal-starter-dynamic/src/app/item/item.component.ts
+++ universal-starter-dynamic/src/app/item/item.component.ts
@@ -12,6 +12,9 @@ import {Subscription} from 'rxjs/Subscription';
 import {Item} from '../item';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {isPlatformBrowser} from '@angular/common';
+import {TransferState, makeStateKey} from '@angular/platform-browser';
+
+const PRICE_KEY = makeStateKey < string > ('price');
 
 @Component({selector: 'app-item', templateUrl: './item.component.html', styleUrls: ['./item.component.css']})
 export class ItemComponent implements OnDestroy {
@@ -25,7 +28,7 @@ export class ItemComponent implements OnDestroy {
 
   private modalService : NgbModal;
 
-  constructor(private dummyService : DummyService, @Inject(PLATFORM_ID)private platformId : Object, private injector : Injector) {
+  constructor(private dummyService : DummyService, @Inject(PLATFORM_ID)private platformId : Object, private injector : Injector, private state : TransferState) {
     this.initPriceSubscription();
     // Hack to save from > Error: No component factory found for NgbModalBackdrop
     if (isPlatformBrowser(this.platformId)) {
@@ -42,14 +45,22 @@ export class ItemComponent implements OnDestroy {
   }
 
   initPriceSubscription() {
-    this.loading = true;
-    this.priceSubscription = this
-      .dummyService
-      .getPrice()
-      .subscribe(p => {
-        this.price = p;
-        this.loading = false;
-      })
+    this.price = this
+      .state
+      .get(PRICE_KEY, 'Not Available');
+    if (this.price === '' || this.price === 'Not Available') {
+      this.loading = true;
+      this.priceSubscription = this
+        .dummyService
+        .getPrice()
+        .subscribe(p => {
+          this.price = p;
+          this
+            .state
+            .set(PRICE_KEY, p);
+          this.loading = false;
+        });
+    }
   }
 
   ngOnDestroy() {
