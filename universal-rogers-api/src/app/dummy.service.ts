5c0f54c53f69e9066b168581cafeed93d52622c6
Obhi, Jagpreet <jagpreet_obhi@manulife.com>
2018-02-09 12:13:57 -0500

    getting the headers

--- universal-rogers-api/src/app/dummy.service.ts
+++ universal-rogers-api/src/app/dummy.service.ts
@@ -3,18 +3,34 @@ import {Observable} from 'rxjs/Observable';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {catchError, map, tap} from 'rxjs/operators';
 import {of} from 'rxjs/observable/of';
+import {Injector} from '@angular/core';
+import {REQUEST} from '@nguniversal/express-engine/tokens';
+import * as xhr2 from 'xhr2';
 
-const httpOptions = {
-        headers: new HttpHeaders({'Content-Type': 'application/json'})
-};
+// HACK - enables setting cookie header xhr2.prototype._restrictedHeaders.cookie
+// = false;
 
 @Injectable()
 export class DummyService {
 
-        constructor(private http : HttpClient) {}
+        constructor(private http : HttpClient, private injector : Injector) {}
 
         fetchData(endpoint, payload) : Observable < Object > {
-                return this.http.post < Object > (endpoint, payload).pipe(tap((result) => console.log('result-->', result)), catchError(this.handleError('error', [])));;
+                const req = this
+                        .injector
+                        .get(REQUEST);
+                console.log('=======  COMPLETE HEADER ========');
+                console.log(req.headers.cookie);
+                let headers = new HttpHeaders({'Content-Type': 'application/json'});
+                // if (req.headers.cookie) {         const cookies = req .headers  .cookie
+                // .split(';'); cookies.forEach(kv => { const kvArray = kv.split('='); headers =
+                // headers.append(kvArray[0].trim(), kvArray[1]);         }); }
+                headers = headers.append('PLAY_SESSION', req.headers.cookie.substring(req.headers.cookie.indexOf('PLAY_SESSION')).substring(13).trim());
+                console.log('------------------------');
+                console.log(headers.get('PLAY_SESSION'));
+                console.log('------------------------');
+                debugger;
+                return this.http.post < Object > (endpoint, payload, {headers, withCredentials: true}).pipe(tap((result) => console.log('result-->', result)), catchError(this.handleError('error', [])));;
         }
 
         /*
@@ -27,7 +43,7 @@ export class DummyService {
                 return(error : any): Observable < T > => {
 
                         // TODO: send the error to remote logging infrastructure
-                        console.error(error); // log to console instead
+                        console.error(JSON.stringify(error)); // log to console instead
 
                         // TODO: better job of transforming error for user consumption
                         console.log(`${operation} failed: ${error.message}`);
